a1ae60b9906b9bf26aa22d9264bf1981afe9597b
kenny wan <kennethzwan@gmail.com>
2022-11-18 21:50:33 -0800

    Create Profile screen and add to tab navigator

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import MapScreen from './Screen/MapScreen';
-import GalleryScreen from './Screen/GalleryScreen';
-import SubmissionScreen from './Screen/SubmissionScreen';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import MapScreen from "./Screen/MapScreen";
+import GalleryScreen from "./Screen/GalleryScreen";
+import SubmissionScreen from "./Screen/SubmissionScreen";
+import ProfileScreen from "./Screen/ProfileScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Gallery" component={GalleryScreen} />
         <Tab.Screen name="Submission" component={SubmissionScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
