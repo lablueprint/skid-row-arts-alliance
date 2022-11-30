69e16f39ec5e89b8089a3258ec6288c50fabf126
kenny wan <kennethzwan@gmail.com>
2022-11-29 22:24:57 -0800

    added clear button to clear input fields
    prints inputs below clear button
    
    Co-authored-by: Juliet Zhang <zhangjuliet@users.noreply.github.com>

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from "@react-navigation/native";
-import MapScreen from "./Screen/MapScreen";
-import GalleryScreen from "./Screen/GalleryScreen";
-import SubmissionScreen from "./Screen/SubmissionScreen";
-import ProfileScreen from "./Screen/ProfileScreen";
-import SignUpScreen from "./Screen/SignUpScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import MapScreen from './Screen/MapScreen';
+import GalleryScreen from './Screen/GalleryScreen';
+import SubmissionScreen from './Screen/SubmissionScreen';
+import ProfileScreen from './Screen/ProfileScreen';
+import SignUpScreen from './Screen/SignUpScreen';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -26,8 +26,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Sign Up" component={SignUpScreen} />
         <Stack.Screen name="Home" component={HomeStackScreen} />
+        <Stack.Screen name="Sign Up" component={SignUpScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
