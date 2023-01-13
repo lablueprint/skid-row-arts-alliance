277e4185ed337a4e81c7ae04a1f70810b78b0cb9
Andrew <andhuynh24@gmail.com>
2023-01-12 17:18:24 -0800

    initalized zines card and screen

---
+++ mobile/Components/ZinesCard.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {
+  Card, Title, Paragraph,
+} from 'react-native-paper';
+
+function ZinesCard() {
+  return (
+    <Card>
+      <Title>Zine Title</Title>
+      <Paragraph>Release Date</Paragraph>
+    </Card>
+  );
+}
+
+export default ZinesCard;
