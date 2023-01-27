8f059defec1991abf2480c028ebd5583276e991b
Caroline DebBaruah <cdebbaruah@gmail.com>
2023-01-26 21:27:51 -0800

    create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const socialSchema = new mongoose.Schema({
+  platform: {
+    required: true,
+    type: String,
+  },
+  accountTag: {
+    required: true,
+    type: String,
+  },
+});
+
+// Model schema to validate and structure user info
+const userSchema = new mongoose.Schema({
+  email: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
+  firstName: {
+    required: true,
+    type: String,
+  },
+  lastName: {
+    required: false,
+    type: String,
+  },
+  socialMedia: {
+    required: true,
+    type: socialSchema,
+  },
+  profilePicture: {
+    required: true,
+    type: String,
+  },
+  savedEvents: {
+    required: true,
+    type: [String],
+  },
+  savedArtwork: {
+    required: true,
+    type: [String],
+  },
+  userArtwork: {
+    required: true,
+    type: [String],
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
