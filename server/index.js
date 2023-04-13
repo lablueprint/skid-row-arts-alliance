58365e7968328c0d86d5c4f10ba9e2e7736d1b6c
James He <he.is.james7@gmail.com>
2023-04-13 01:02:58 -0700

    Revert "Merge branch 'zines-reader' into portal-gallery-zines"
    This reverts commit 7dc39003081aac4131764cf664fc9e6a93fd8947, reversing
    changes made to 4934b6f812f8687bdffd351e6ef83312eb867f3e.

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ const eventRouter = require('./routes/eventRoutes');
 const resourceRouter = require('./routes/resourceRoutes');
 const submissionRouter = require('./routes/submissionRoutes');
 const userRouter = require('./routes/userRoutes');
-const zineRouter = require('./routes/zineRoutes');
 
 // Start the Node Express server
 const app = express();
@@ -37,7 +36,6 @@ app.use('/event', eventRouter);
 app.use('/resource', resourceRouter);
 app.use('/submissions', submissionRouter);
 app.use('/user', userRouter);
-app.use('/zine', zineRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
