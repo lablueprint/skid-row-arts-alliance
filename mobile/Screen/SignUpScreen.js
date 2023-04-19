3536c02e087d1e203733c2cd8f75d4ccd47f5362
Andrew Huynh <61716827+andhuynh@users.noreply.github.com>
2023-04-18 21:04:22 -0700

    "Added different pages to sign up screen"

--- mobile/Screen/SignUpScreen.js
+++ mobile/Screen/SignUpScreen.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import React, { useState } from 'react';
 import {
-  StyleSheet, Text, TextInput, View, Button,
+  StyleSheet, Text, TextInput, View, Button, Alert, Keyboard,
 } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import * as SecureStore from 'expo-secure-store';
@@ -28,16 +28,63 @@ async function save(key, value) {
 }
 
 function SignUpScreen({ navigation }) {
-  const [name, onChangeName] = React.useState('');
+  const [page, setPage] = React.useState(1);
+
+  const [firstName, onChangeFirstName] = React.useState('');
+  const [lastName, onChangeLastName] = React.useState('');
+
   const [email, onChangeEmail] = React.useState('');
+  const [password, onChangePassword] = React.useState('');
+  const [confirmPassword, onChangeConfirmPassword] = React.useState('');
+
+  const [bio, onChangeBio] = React.useState('');
+
   const [socialPlatform, setSocialPlatform] = useState('');
   const [socialTag, onChangeSocialTag] = React.useState('');
+
+  // Check email is proper format
+  const validateEmail = (text) => {
+    const reg = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w\w+)+$/;
+    if (reg.test(text) === false) {
+      return false;
+    }
+    return true;
+  };
+
+  // Check name inputs are filled before moving to next screen
+  const checkNameInputs = () => {
+    if (firstName === '') {
+      Alert.alert('Please enter a first name to proceed');
+    } else {
+      return true;
+    }
+    return false;
+  };
+
+  // Check account inputs are filled appropriately before moving to next screen
+  const checkAccountInputs = () => {
+    if (email === '') {
+      Alert.alert('Please enter an email to proceed');
+    } else if (!validateEmail(email)) {
+      Alert.alert('Please enter a valid email to proceed');
+    } else if (password === '') {
+      Alert.alert('Please enter a password to proceed');
+    } else if (password.length < 6) {
+      Alert.alert('Password must be longer than 5 characters');
+    } else if (password !== confirmPassword) {
+      Alert.alert('Password Confirmation does not match Password');
+    } else {
+      return true;
+    }
+    return false;
+  };
 
   // Store input and navigate to Home screen
   const handleSignUp = async () => {
     try {
       const userData = {
-        userName: name,
+        userFirstName: firstName,
+        userLastName: lastName,
         userEmail: email,
         userSocialPlatform: socialPlatform,
         userSocialTag: socialTag,
@@ -49,28 +96,151 @@ function SignUpScreen({ navigation }) {
     }
   };
 
+  if (page === 1) {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text>
+            First Name
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeFirstName}
+            value={firstName}
+          />
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text>
+            Last Name
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeLastName}
+            value={lastName}
+          />
+        </View>
+        <Button
+          title="Next"
+          onPress={() => {
+            if (checkNameInputs()) {
+              Keyboard.dismiss();
+              setPage(2);
+            }
+          }}
+        />
+      </View>
+    );
+  }
+
+  if (page === 2) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text>
+              Email
+            </Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeEmail}
+              value={email}
+              autoCapitalize={false}
+            />
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text>
+              Password
+            </Text>
+            <TextInput
+              secureTextEntry
+              style={styles.input}
+              onChangeText={onChangePassword}
+              value={password}
+              autoCapitalize={false}
+            />
+            <Text>6 or more characters</Text>
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text>
+              Confirm Password
+            </Text>
+            <TextInput
+              secureTextEntry
+              style={styles.input}
+              onChangeText={onChangeConfirmPassword}
+              value={confirmPassword}
+              autoCapitalize={false}
+            />
+          </View>
+          <Button
+            title="Back"
+            onPress={() => {
+              setPage(1);
+            }}
+          />
+          <Button
+            title="Next"
+            onPress={() => {
+              if (checkAccountInputs()) {
+                Keyboard.dismiss();
+                setPage(3);
+              }
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+
+  if (page === 3) {
+    return (
+      <View style={styles.container}>
+        <Text>Add a Profile Picture</Text>
+        <Button
+          title="Back"
+          onPress={() => {
+            setPage(2);
+          }}
+        />
+        <Button
+          title="Skip For Now"
+          onPress={() => {
+            setPage(4);
+          }}
+        />
+
+      </View>
+    );
+  }
+
+  if (page === 4) {
+    return (
+      <View style={styles.container}>
+        <Text>What do you want people to know about you?</Text>
+        <Text>Share anything you would like!</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeBio}
+          value={bio}
+        />
+        <Button
+          title="Back"
+          onPress={() => {
+            setPage(3);
+          }}
+        />
+        <Button
+          title="Skip For Now"
+          onPress={() => {
+            setPage(5);
+          }}
+        />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text>
-          Name
-        </Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeName}
-          value={name}
-        />
-      </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text>
-          Email
-        </Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeEmail}
-          value={email}
-        />
-      </View>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <Text>
           Social Media Platform
@@ -96,9 +266,16 @@ function SignUpScreen({ navigation }) {
         />
       </View>
       <Button
+        title="Back"
+        onPress={() => {
+          setPage(1);
+        }}
+      />
+      <Button
         title="Sign Up"
         onPress={() => {
-          save('name', name);
+          save('firstName', firstName);
+          save('lastName', lastName);
           save('email', email);
           save('socialTag', socialTag);
           save('socialPlatform', socialPlatform);
