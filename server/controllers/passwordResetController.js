6c92909a9cc09d79cd941ebc9f2a844c2da8d9bf
Siddharth Nandy <sidd.nandy@gmail.com>
2023-05-04 20:46:44 -0700

    added nodemailer transporter

--- server/controllers/passwordResetController.js
+++ server/controllers/passwordResetController.js
@@ -1,7 +1,19 @@
 const nodemailer = require('nodemailer');
 const User = require('../models/userModel');
+// import { MAIL_USER, MAIL_PASS } from '@env';
 
 const createResetCode = async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    port: 465,               // true for 465, false for other ports
+    host: "smtp.gmail.com",
+       auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+         },
+    secure: true,
+    });
+    console.log({user: process.env.MAIL_USER, pass: process.env.MAIL_PASS});
+    
   // generate a random integer between 0 and 9999
   const randomInt = Math.floor(Math.random() * 10000);
   // pad the integer with leading zeros to make it a 4-digit string
