dcd50af4ad88afc3c6b65490efe06282d3835ba3
Siddharth Nandy <sidd.nandy@gmail.com>
2023-05-09 20:43:17 -0700

    add reset code check

--- server/controllers/passwordResetController.js
+++ server/controllers/passwordResetController.js
@@ -1,76 +1,67 @@
 const nodemailer = require('nodemailer');
 const User = require('../models/userModel');
 
-const createResetCode = async (req, res) => {
-  const transporter = nodemailer.createTransport({
-    port: 465,               // true for 465, false for other ports
-    host:'smtp.gmail.com',
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-    secure: true,
-  });
+const transporter = nodemailer.createTransport({
+  port: 465,               // true for 465, false for other ports
+  host:'smtp.gmail.com',
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+  secure: true,
+});
 
+const createResetCode = async (req, res) => {
   // generate a random integer between 0 and 9999
   const randomInt = Math.floor(Math.random() * 10000);
   // pad the integer with leading zeros to make it a 4-digit string
   const passwordResetCode = String(randomInt).padStart(4, '0');
-  console.log({user: process.env.MAIL_USER, pass: process.env.MAIL_PASS, passwordResetCode});
 
   try {
+    // Add reset code to user record
+    const user = await User.findOneAndUpdate({ email: req.body.email }, { passwordResetCode });
+    res.json({
+      email: user.email,
+      error: null,
+    });
+    // Send email with reset code
     transporter.sendMail({
       to: req.body.email,
       subject: 'Skid Row Arts Alliance - Account Password Reset',
       text: `Your account password reset code is ${passwordResetCode}`,
     });
   } catch (err) {
-    console.log('transporter error');
-    console.log(err);
-    res.json({
-      action: 'createResetCode',
+    console.error(err);
+    res.status(err.statusCode ? err.statusCode : 400).json({
+      email: null,
       error: err
     });
     return;
   }
-
-
-  User.findOneAndUpdate({ email: req.body.email }, { passwordResetCode }, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.json({
-        email: data.email,
-        action: 'createResetCode',
-        error: err
-      });
-    } else {
-      res.json({
-        email: data.email,
-        action: 'createResetCode',
-        error: null,
-      });
-    }
-  });
 };
 
 const deleteResetCode = async (req, res) => {
-  console.log(req.body);
-  User.findOneAndUpdate({ email: req.body.email }, { passwordResetCode: '' }, (err, data) => {
-    if (err) {
-      console.error(err);
+  try {
+    const user = await User.findOne({ email: req.body.email, passwordResetCode: req.body.passwordResetCode });
+    if (user) {
+      await User.findByIdAndUpdate(user._id, { passwordResetCode: null });
       res.json({
-        email: data.email ? data.email : null,
-        action: 'deleteResetCode',
-        error: err,
+        email: userRes.email,
+        error: null,
       })
     } else {
       res.json({
-        email: data.email,
-        action: 'deleteResetCode',
-        error: null,
-      })
+        email: req.body.email,
+        error: 'Incorrect email or reset code',
+      });
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(err.statusCode ? err.statusCode : 400).json({
+      email: null,
+      error: err,
+    })
+  }
 };
 
 module.exports = {
