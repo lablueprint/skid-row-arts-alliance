3d8532b273fb68b6b05ce80839500a8204749f2f
Siddharth Nandy <sidd.nandy@gmail.com>
2023-05-16 20:13:38 -0700

    delete user sign out route (unused)

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,6 @@ const authController = require('../controllers/authController');
 // mobile sign in/sign up
 authRouter.post('/user-sign-up', authController.userSignUp);
 authRouter.post('/user-sign-in', authController.userSignIn);
-authRouter.post('/user-sign-out', passport.authenticate('jwt', { session: false }), authController.userSignOut);
 
 // admin sign in/sign up
 authRouter.post('/admin-sign-up', authController.adminSignUp);
