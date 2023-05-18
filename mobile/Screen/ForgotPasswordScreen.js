26504ccecd49e596955547389af4d465279a2493
Andrew Huynh <61716827+andhuynh@users.noreply.github.com>
2023-05-17 22:05:31 -0700

    added basic page functionality

--- mobile/Screen/ForgotPasswordScreen.js
+++ mobile/Screen/ForgotPasswordScreen.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import { URL } from '@env';
 import {
-  StyleSheet, Text, TextInput, View, Button, Alert, TouchableOpacity, Dimensions,
+  StyleSheet, Text, TextInput, View, Image, Alert, TouchableOpacity,
 } from 'react-native';
 import { login } from '../redux/sliceAuth';
+
+const BackButton = require('../assets/backArrow.png');
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -15,6 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
+    borderColor: 'green',
   },
   signInContainer: {
     flexDirection: 'column',
@@ -22,6 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
+    borderColor: 'blue',
   },
   input: {
     height: 40,
@@ -38,7 +42,12 @@ const styles = StyleSheet.create({
     width: '100%',
     justifyContent: 'space-between',
     borderWidth: 1,
+    borderColor: 'red',
     margin: 20,
+  },
+  codeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   button: {
     height: 40,
@@ -49,6 +58,16 @@ const styles = StyleSheet.create({
     marginVertical: '5%',
     backgroundColor: '#4C4C9B',
   },
+  backButton: {
+    width: 20,
+    height: 20,
+  },
+  backButtonPosiion: {
+    alignItems: 'flex-start',
+    width: '100%',
+    marginLeft: '10%',
+    marginTop: '10%',
+  },
   link: {
     textDecorationLine: 'underline',
   },
@@ -56,6 +75,39 @@ const styles = StyleSheet.create({
 
 function ForgotPasswordScreen({ navigation }) {
   const [email, onChangeEmail] = useState('');
+  const [password, onChangePassword] = useState('');
+  const [hiddenPassword, onChangeHiddenPassword] = useState('');
+  const [confirmPassword, onChangeConfirmPassword] = useState('');
+  const [bool, setBool] = useState(false);
+
+  const handleBlur = () => {
+    setBool(true);
+  };
+
+  const handleFocus = () => {
+    setBool(false);
+  };
+
+  const handleChangePassword = (newText) => {
+    const lastLetter = newText.slice(-1);
+    if (newText.length > password.length) {
+      onChangePassword(password + lastLetter);
+    } else if (newText.length < password.length) {
+      onChangePassword(password.slice(0, newText.length));
+    } else if (newText === '') {
+      onChangePassword('');
+      setBool(false);
+    }
+    let newTextWithDots = '';
+    newText.split('').forEach((char, index, array) => {
+      if (index === array.length - 1) {
+        newTextWithDots += char;
+      } else {
+        newTextWithDots += '•';
+      }
+    });
+    onChangeHiddenPassword(newTextWithDots);
+  };
   const [page, setPage] = useState(1);
 
   const validateEmail = (text) => {
@@ -75,8 +127,112 @@ function ForgotPasswordScreen({ navigation }) {
     return false;
   };
 
+  if (page === 2) {
+    return (
+      <View style={styles.mainContainer}>
+        <View style={styles.backButtonPosiion}>
+          <TouchableOpacity
+            onPress={() => {
+              setPage(1);
+            }}
+          >
+            <Image
+              source={BackButton}
+              style={styles.backButton}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.signInContainer}>
+          <Text>Verify Your Email</Text>
+          <Text>Please enter the 4-digit code sent to yourname@gmail.com.</Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeEmail}
+              value={email}
+              autoCapitalize={false}
+            />
+            <TouchableOpacity
+              onPress={() => {
+                setPage(2);
+              }}
+            >
+              <Text style={styles.link}>Resend code?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                setPage(3);
+              }}
+            >
+              <Text style={styles.buttonText}>Verify</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  if (page === 3) {
+    return (
+      <View style={styles.mainContainer}>
+        <View style={styles.signInContainer}>
+          <Text>Create New Password</Text>
+          <Text>Your new password needs to be different from your old one!</Text>
+          <View style={styles.inputContainer}>
+            <Text>
+              New Password
+            </Text>
+            <TextInput
+              secureTextEntry={bool}
+              style={styles.input}
+              value={hiddenPassword}
+              onChangeText={handleChangePassword}
+              autoCapitalize={false}
+              onBlur={handleBlur}
+              onFocus={handleFocus}
+            />
+            <Text>6 or more characters</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text>
+              Confirm Password
+            </Text>
+            <TextInput
+              secureTextEntry
+              style={styles.input}
+              onChangeText={onChangeConfirmPassword}
+              value={confirmPassword}
+              autoCapitalize={false}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              navigation.navigate('Sign In');
+            }}
+          >
+            <Text style={styles.buttonText}>Save</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.mainContainer}>
+      <View style={styles.backButtonPosiion}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Sign In');
+          }}
+        >
+          <Image
+            source={BackButton}
+            style={styles.backButton}
+          />
+        </TouchableOpacity>
+      </View>
       <View style={styles.signInContainer}>
         <Text>Forgot Password?</Text>
         <Text>Please enter your email address to receive a verification code.</Text>
@@ -100,7 +256,7 @@ function ForgotPasswordScreen({ navigation }) {
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => {
-              navigation.navigate('Forgot Password');
+              setPage(2);
             }}
           >
             <Text style={styles.link}>Already have a code?</Text>
@@ -108,7 +264,6 @@ function ForgotPasswordScreen({ navigation }) {
         </View>
       </View>
     </View>
-
   );
 }
 
