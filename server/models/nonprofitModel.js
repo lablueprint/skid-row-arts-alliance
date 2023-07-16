e2948d47f1c3af49bb8c90c74c97799b333ca796
James He <he.is.james7@gmail.com>
2023-07-15 20:46:00 -0700

    created api for nonprofits

---
+++ server/models/nonprofitModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const nonprofitSchema = new mongoose.Schema({
+  image: {
+    required: false,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  organizationTitle: {
+    required: true,
+    type: String,
+  },
+  phoneNumber: {
+    required: true,
+    type: String,
+  },
+  website: {
+    required: true,
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Nonprofit', nonprofitSchema);
